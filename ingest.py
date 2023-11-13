f6929646addd9f8b2c27c3e83ab043ef95854ce6
mads.brodthagen <mads.brodthagen@cmnavigator.com>
2023-11-13 14:45:23 +0100

    v2

--- ingest.py
+++ ingest.py
@@ -4,8 +4,9 @@ from langchain.text_splitter import CharacterTextSplitter
 import faiss
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
+import openai
 import pickle
-
+openai = OpenAIEmbeddings(openai_api_key="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")
 
 # Here we load in the data in the format that Notion exports it in.
 ps = list(Path("Notion_DB/").glob("**/*.md"))
@@ -13,13 +14,15 @@ ps = list(Path("Notion_DB/").glob("**/*.md"))
 data = []
 sources = []
 for p in ps:
-    with open(p) as f:
+    with open(p, encoding="utf8") as f:
         data.append(f.read())
     sources.append(p)
+
 
 # Here we split the documents, as needed, into smaller chunks.
 # We do this due to the context limits of the LLMs.
 text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
+
 docs = []
 metadatas = []
 for i, d in enumerate(data):
@@ -28,9 +31,16 @@ for i, d in enumerate(data):
     metadatas.extend([{"source": sources[i]}] * len(splits))
 
 
+
+
 # Here we create a vector store from the documents and save it to disk.
-store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
-faiss.write_index(store.index, "docs.index")
-store.index = None
-with open("faiss_store.pkl", "wb") as f:
-    pickle.dump(store, f)
+store = FAISS.from_texts(docs, OpenAIEmbeddings(openai_api_key = "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"), metadatas=metadatas)
+
+#faiss.write_index(store.index, "docs.index")
+#store.index = None
+#with open("faiss_store.pkl", "wb") as f:
+#    pickle.dump(store, f)
+
+
+print(store.index)
+
